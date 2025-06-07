0db608895f7c842b244aa4e1515aaeb681db654d
Alex <laxus1195@gmail.com>
2025-06-07 14:00:57 -0500

    Añado el argumento de enemigo en el método abstracto atacarEspecial

--- src/Logic/Magos/MagoAbisal.java
+++ src/Logic/Magos/MagoAbisal.java
@@ -6,6 +6,6 @@ public class MagoAbisal extends Personaje{
 
     @Override
     public void atacarEspecial(Personaje enemigo) {
-
+        System.out.println("Mago abisal realizó un ataque especial.");
     }
 }
